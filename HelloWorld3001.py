4c8cb6ce4d19ed85cff20d32874b5da9b8d81d91
6024212003 <44814491+6024212003@users.noreply.github.com>
2018-11-06 10:34:21 -0700

    Add files via upload

---
+++ HelloWorld3001.py
@@ -0,0 +1,11 @@
+print("Hello World")
+var = 2018
+print("today is monday the 5th of november" , var)
+
+print(7 + 8)
+print(7 - 8)
+print(7*8)
+print(58/5)
+print(5**2)
+
+print("my name is Florin Postelnicu \n"* 5)k
